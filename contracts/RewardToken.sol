79f8ab6367e9f8d9281d6b59750e74a1cb06cb99
quantroc <105766206+quantroc@users.noreply.github.com>
2022-11-11 11:31:28 +0700

    Update RewardToken.sol

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -3,19 +3,16 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-//0x7d96Af8B11d2F987e702313df9cc1FdEBdAc0986
+
 contract RewardToken is ERC20, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC20("Quan", "QUAN") {
+    constructor() ERC20("Quan","QT"){
         _mint(msg.sender, 1000000 * 10 ** decimals());
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    // function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-    //     _mint(to, amount);
-    // }
-    function mint(address to, uint256 amount) public {
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 }
